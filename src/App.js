d07e15c6e455f8eada22c5604cf02713dd850262
Tarik-Talic <talic.tarik@gmail.com>
2023-06-29 18:55:18 +0200

    DarkMode Toggle

--- src/App.js
+++ src/App.js
@@ -2,13 +2,19 @@ import "./App.css";
 import Header from "./components/Header";
 import Card from "./components/Card";
 import Search from "./components/Search";
-
 import { useState } from "react";
-// test
+import { createContext } from "react";
+
+export const ThemeContext = createContext(null);
 
 function App() {
+  const [theme, setTheme] = useState("light");
   const [weatherData, setWeatherData] = useState([{}]);
   const [city, setCity] = useState("");
+
+  const toggleTheme = () => {
+    setTheme((currTheme) => (currTheme === "light" ? "dark" : "light"));
+  };
 
   const apiKey = "3a123655a834444cef356139026ca886";
   const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
@@ -25,29 +31,31 @@ function App() {
   };
   console.log(weatherData);
   return (
-    <div className="App">
-      <Header />
-      <Search getWeatherData={getWeatherData} setCity={setCity} city={city} />
-      {!weatherData.main ? (
-        <div className="containerTxt">
-          <p className="welcomeTxt">
-            Welcome <br />
-            to <br />
-            Weather App!
-          </p>
-          <p className="infoTxt">Enter a city to find out the weather information.</p>
-        </div>
-      ) : (
-        <Card
-          weatherData={weatherData}
-          cityName={weatherData.name}
-          countryName={weatherData.sys.country}
-          temparature={weatherData.main.temp}
-          icon={weatherData.weather[0].icon}
-          description={weatherData.weather[0].description}
-        />
-      )}
-    </div>
+    <ThemeContext.Provider value={{ theme, toggleTheme }}>
+      <div className="App" id={theme}>
+        <Header toggleTheme={toggleTheme} theme={theme} />
+        <Search getWeatherData={getWeatherData} setCity={setCity} city={city} />
+        {!weatherData.main ? (
+          <div className="containerTxt">
+            <p className="welcomeTxt">
+              Welcome <br />
+              to <br />
+              Weather App!
+            </p>
+            <p className="infoTxt">Enter a city to find out the weather information.</p>
+          </div>
+        ) : (
+          <Card
+            weatherData={weatherData}
+            cityName={weatherData.name}
+            countryName={weatherData.sys.country}
+            temparature={weatherData.main.temp}
+            icon={weatherData.weather[0].icon}
+            description={weatherData.weather[0].description}
+          />
+        )}
+      </div>
+    </ThemeContext.Provider>
   );
 }
 
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,11 @@
 import "../styles/Header.css";
 import Logo from "../assets/WeatherLogoApp.png";
+import Switch from "react-switch";
 
-export default function Header() {
+export default function Header(props) {
   return (
     <div className="nav">
+      <Switch onChange={props.toggleTheme} checked={props.theme === "dark"} />
       <img src={Logo} />
       <h1>Weather App</h1>
     </div>
