06a466e5e2e12d08e84c3181d3934de8a05b07bd
Tarik-Talic <talic.tarik@gmail.com>
2023-06-29 19:07:16 +0200

    Bug fixes

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,9 +5,14 @@ import Switch from "react-switch";
 export default function Header(props) {
   return (
     <div className="nav">
-      <Switch onChange={props.toggleTheme} checked={props.theme === "dark"} />
-      <img src={Logo} />
-      <h1>Weather App</h1>
+      <div className="toggle_switch">
+        <label>{props.theme === "light" ? "Light Mode" : "Dark Mode"}</label>
+        <Switch onChange={props.toggleTheme} checked={props.theme === "dark"} />
+      </div>
+      <span className="logo">
+        <img src={Logo} />
+        <h1>Weather App</h1>
+      </span>
     </div>
   );
 }
